e4914a37516c9852160f0ada76e6790f8ca8c4e1
Ismail Rabie <108563945+esmail830@users.noreply.github.com>
2025-09-04 11:28:34 -0700

    Update supabase_config.dart

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,5 +1,5 @@
 // config/supabase_config.dart
 class PSupabaseConfig {
-  static const String supabaseUrl = 'https://yryvvelcmymifkjtfenf.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyeXZ2ZWxjbXltaWZranRmZW5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwNzgwNTgsImV4cCI6MjA3MDY1NDA1OH0.1tP2k6Hm009sP_7GwBkKBXHGD1QEg49W5YVQYtAPDRU';
+  static const String supabaseUrl = 'supabase api';
+  static const String supabaseAnonKey = 'api key';
 } 
